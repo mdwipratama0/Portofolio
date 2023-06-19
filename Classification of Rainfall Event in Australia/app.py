7c63ecd3da71f286769878e96aa29b9e2c6565a8
M. Dwi Pratama <133756689+mdwipratama0@users.noreply.github.com>
2023-06-19 19:33:37 +0700

    Add files via upload

---
+++ Classification of Rainfall Event in Australia/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, url_for
+import pickle
+import pandas as pandas
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the pickled KNN model
+model = pickle.load(open("/home/raremon/mysite/knn.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    feature = [np.array(float_features)]
+    prediction = model.predict(feature)
+    if prediction[0] == 0:
+        prediction = 'Besok cuaca akan cerah'
+        image_filename = 'sun_image.png'
+        comment = 'Sebaiknya anda memakai sunblock.'
+    else :
+        prediction = 'Besok cuaca akan hujan'
+        image_filename = 'rain_image.png'
+        comment = 'Sebaiknya anda membawa payung.'
+    
+    image_path = url_for('static', filename='images/' + image_filename)
+
+    return render_template('index.html', RainTomorrow = "{}".format(prediction), image_path=image_path, comment=comment)
+
+if __name__ == "__main__":
+    # Fit the scaler on sample data
+    app.run(debug=True)
